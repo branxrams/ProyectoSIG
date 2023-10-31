7c777ce5b70d4fd114c52bd74e4ff4fc37090375
Kevin Rangel <krangelj@unicartagena.edu.co>
2023-10-30 22:13:01 -0500

    Fix: gtts import

--- src/app/textToSpech.py
+++ src/app/textToSpech.py
@@ -1,11 +1,24 @@
-from gtts import gTTS
 import os
 
-def texto_a_voz(texto, archivo_salida):
-    tts = gTTS(text=texto, lang='es')
+import gtts
 
-    # Guarda la voz generada en un archivo de audio
-    tts.save(archivo_salida)
 
-    # Reproduce el archivo de audio (opcional)
-    os.system(f"start {archivo_salida}")  # En Windows
+def texto_a_voz(texto: str, archivo_salida: str):
+    """Convierte un texto a voz y lo guarda en un archivo de audio.
+
+    Parameters
+    ----------
+    texto : str
+        Texto a convertir a voz.
+    archivo_salida : str
+        Dirección del archivo de salida.
+    """
+    tts = gtts.gTTS(text=texto, lang="es")
+
+    try:
+        tts.save(archivo_salida)
+    except gtts.gTTSError:
+        print("Error al generar el archivo de audio")
+    else:
+        # Reproduce el archivo de audio (opcional)
+        os.system(f"start {archivo_salida}")  # En Windows
