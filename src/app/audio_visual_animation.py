3854d6546cf432503e938763ffd222030cdac0d8
Brayan Alexander Salazar Reyes <bsalazarreyes88@gmail.com>
2023-11-01 12:06:11 -0500

    build(deps): update .gitignore, pyproject.toml and audio_visual_animation.py

--- src/app/audio_visual_animation.py
+++ src/app/audio_visual_animation.py
@@ -9,8 +9,9 @@
 
 import cv2
 import numpy as np
+from moviepy.editor import AudioFileClip, VideoFileClip, VideoClip
 from pydub import AudioSegment
-from moviepy.editor import VideoFileClip, AudioFileClip
+
 
 class AudioImageAnimator:
     def __init__(self, image_path, audio_path, output_path):
@@ -24,8 +25,10 @@ class AudioImageAnimator:
         audio_duration = len(self.audio) / 1000
         num_frames = int(frame_rate * audio_duration)
 
-        fourcc = cv2.VideoWriter_fourcc(*'XVID')
-        video = cv2.VideoWriter(self.output_path, fourcc, frame_rate, (image_width, image_height))
+        fourcc = cv2.VideoWriter_fourcc(*"XVID")
+        video = cv2.VideoWriter(
+            self.output_path, fourcc, frame_rate, (image_width, image_height)
+        )
 
         for i in range(num_frames):
             start_time = i * (len(self.audio) / num_frames)
@@ -47,12 +50,13 @@ class AudioImageAnimator:
             ret, frame = cap.read()
             if not ret:
                 break
-            cv2.imshow('Video', frame)
-            if cv2.waitKey(30) & 0xFF == ord('q'):
+            cv2.imshow("Video", frame)
+            if cv2.waitKey(30) & 0xFF == ord("q"):
                 break
 
         cap.release()
         cv2.destroyAllWindows()
+
 
 class VideoAudioCombiner:
     """
@@ -66,6 +70,7 @@ class VideoAudioCombiner:
     Returns:
         None
     """
+
     def __init__(self, video_path, audio_path):
         self.video_path = video_path
         self.audio_path = audio_path
@@ -76,21 +81,52 @@ class VideoAudioCombiner:
 
         video_clip = video_clip.set_audio(audio_clip)
 
-        video_clip.write_videofile(output_sound_path, codec='libx264')
+        video_clip.write_videofile(output_sound_path, codec="libx264")
 
         video_clip.close()
         audio_clip.close()
 
-if __name__ == "__main__":
-    image_path = 'imagen_prueba.jpg'
-    audio_path = 'audio_prueba.mp3'
-    output_path = 'video_prueba.mp4'
-    output_sound_path = 'video_con_audio.mp4'
+class TalkingHeadAnimator:
+    def __init__(self, image_path, audio_path, output_path):
+        self.image = cv2.imread(image_path)
+        self.audio: AudioSegment = AudioSegment.from_file(audio_path)
+        self.output_path = output_path
 
-    animator = AudioImageAnimator(image_path, audio_path, output_path)
-    animator.create_animation()
-    #animator.show_animation()
-    
+    def animate_mouth(self, t):
+        audio_chunk = self.audio[t * 1000:(t + 1) * 1000]
+        audio_volume = audio_chunk.rms
+
+        # Determina el radio del círculo de la boca en función del volumen del audio
+        mouth_radius = int(5 + audio_volume / 400) # para un diametro de 10
+
+        # Convierte la imagen a formato BGR
+        animated_image = cv2.cvtColor(self.image, cv2.COLOR_RGB2BGR)
+
+        # Dibuja un círculo en la imagen sin cambiar el color
+        cv2.circle(animated_image, (211, 268), mouth_radius, (0, 0, 0), thickness=cv2.FILLED)
+
+        return animated_image
+
+    def create_animation(self):
+        video_clip = VideoClip(self.animate_mouth, duration=self.audio.duration_seconds)
+        video_clip.fps = 60  # Establece la velocidad de cuadros por segundo
+        video_clip.set_duration(self.audio.duration_seconds).write_videofile(self.output_path, codec='libx264')
+
+
+
+if __name__ == "__main__":
+    image_path = "prueba1.jpg"
+    audio_path = "audio_prueba.mp3"
+    output_path = "video_prueba.mp4"
+    output_sound_path = "video_con_audio.mp4"
+
+    #animator = AudioImageAnimator(image_path, audio_path, output_path)
+    #animator.create_animation()
+    # animator.show_animation()
+
+    animator_mouth = TalkingHeadAnimator(image_path, audio_path, output_path)
+    animator_mouth.create_animation()
+
     output_sound_path = "video_con_audio.mp4"
 
     combiner = VideoAudioCombiner(output_path, audio_path)
